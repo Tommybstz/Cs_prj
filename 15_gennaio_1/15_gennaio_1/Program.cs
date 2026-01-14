47d80d9bf0ce3a6e17fca331aa65b09b318f1047
Tommybstz <tommaso.bastanzetti@liceischio.edu.it>
2026-01-14 20:34:53 +0100

    Add files via upload

---
+++ 15_gennaio_1/15_gennaio_1/Program.cs
@@ -0,0 +1,33 @@
+﻿public class Program
+{
+    static void Main(string[] args)
+    {
+        //var
+        string name;
+        int scatti_P, scatti_A;
+        double price,base_price,total_price;
+
+        //input
+        Console.WriteLine("Inserire nome utente:");
+        name = Console.ReadLine();
+
+        Console.WriteLine("Inserire numero scatti precedenti:");
+        scatti_P = int.Parse(Console.ReadLine());
+        
+        Console.WriteLine("Inserire numero scatti attuali:");
+        scatti_A = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Inserire prezzo per scatto:");
+        price = double.Parse(Console.ReadLine());
+
+        Console.WriteLine("Inserire prezzo base:");
+        base_price = double.Parse(Console.ReadLine());
+
+        //calcolo prezzo
+        total_price = base_price + (scatti_A - scatti_P) * price;
+
+        //output
+        Console.WriteLine($"la bolletta dell'utente {name} e' di: {total_price} euro");
+
+    }
+}
