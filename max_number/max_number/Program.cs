07ebc8d3c9267242ab789163c07a69315ea6d8c5
Tommy_Bstz.V2 <tommaso.bastanzetti@liceischio.edu.it>
2026-01-17 21:36:14 +0100

    Add files via upload

---
+++ max_number/max_number/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System;
+using System.Linq;
+class Program
+{
+    static void Main()
+    {
+        //var
+        double num;
+        List<double> numbers = new List<double>();
+
+        //input and storing
+        for (int i = 0; i < 3; i++)
+        {
+            Console.WriteLine("inserire un numero");
+            num=double.Parse(Console.ReadLine());
+            numbers.Add(num);
+        }
+        //output max
+        Console.WriteLine($"il valore più grande è: {numbers.Max()}");
+
+    }
+}
