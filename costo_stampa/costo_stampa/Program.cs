df80b8c69f414384531d595cfe3709d80fd5797b
Tommy_Bstz.V2 <tommaso.bastanzetti@liceischio.edu.it>
2026-01-17 21:37:28 +0100

    Add files via upload

---
+++ costo_stampa/costo_stampa/Program.cs
@@ -0,0 +1,30 @@
+﻿class Program
+{
+    static void Main()
+    {
+        //var
+        int pics;
+        double tariff_1;
+        double tariff_2;
+        string result;
+        //input
+        Console.WriteLine("Inserisci il numero di foto da stampare:");
+        pics = int.Parse(Console.ReadLine());
+
+        //price
+        tariff_1 = (pics * 0.5) + 10;
+        tariff_2 = pics * 0.65;
+        
+        if(tariff_1 < tariff_2)
+        {
+            result = "tariffa 1";
+        }
+        else
+        {
+            result = "tariffa 2";
+        }
+
+        //output
+        Console.WriteLine($"La tariffa più conveniente è: {result}");
+    }
+}
