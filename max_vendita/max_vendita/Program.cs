f865cc6fa71f5ffaad9af9806e6dd3a9bcfade08
Tommy_Bstz.V2 <tommaso.bastanzetti@liceischio.edu.it>
2026-01-19 16:52:40 +0100

    Add files via upload

---
+++ max_vendita/max_vendita/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+class Program
+{
+    static void Main(string[] args)
+    {
+        //var
+        int N,dvd;
+        List<int> vendite = new List<int>();
+
+        //input
+        Console.WriteLine("inserisci il numero di giorni:");
+        N = int.Parse(Console.ReadLine());
+
+        //calcolo
+        for (int i =1; i <= N; i++)
+        {
+            Console.WriteLine($"inserisci il numero di dvd venduti nel giorno {i}:");
+            dvd=int.Parse(Console.ReadLine());
+
+            if (dvd > 10)
+            {
+                vendite.Add(i);
+            }
+        }
+
+        //output
+        Console.WriteLine($"i giorni con piu di 10 vendite sono: {string.Join(", ",vendite)}");
+
+
+    }
+}
---
+++ spese_spedizione/spese_spedizione/Program.cs
@@ -0,0 +1,43 @@
+﻿
+class Program
+{
+    static void Main(string[] args)
+    {
+        //var
+        int N;
+        double weight;
+        double shippingCost=0;
+
+        //input
+        Console.WriteLine("inserire il numero di pacchi da spedire");
+        N = int.Parse(Console.ReadLine());
+
+        //cost calculation
+        for (int i = 0; i<N; i++)
+        {
+            //input 2
+            Console.WriteLine($"inserire il peso del pacco numero: {i + 1}");
+            weight = double.Parse(Console.ReadLine());
+
+            //calculation
+            if (weight<=3)
+            {
+                shippingCost += 5;
+            }
+            else if(weight<=10)
+            {
+                shippingCost += 8;
+            }
+            else
+            {
+                shippingCost += 10;
+            }
+        }
+        shippingCost += shippingCost * 0.04;
+
+        //output
+        Console.WriteLine($"il costo totale di spedizione è: {shippingCost} euro");
+
+
+    }
+}
