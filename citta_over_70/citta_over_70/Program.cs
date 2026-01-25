7a31779ffeb1937cc19e797d0d3d3a2f68f646c6
Tommy_Bstz.V2 <tommaso.bastanzetti@liceischio.edu.it>
2026-01-25 14:54:54 +0100

    Add files via upload

---
+++ citta_over_70/citta_over_70/Program.cs
@@ -0,0 +1,49 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        //var
+        int citta,abitanti,over70=0;
+        string nome;
+        double perc;
+
+        //process
+        Console.WriteLine("Inserisci il numero di citta da analizzare:");
+        citta = int.Parse(Console.ReadLine());
+        
+        for (int i = 0; i < citta; i++)
+        {
+            Console.WriteLine($"Inserisci il nome della citta {i + 1}:");
+            nome =Console.ReadLine();
+
+            while (true)
+            {
+                Console.WriteLine($"Inserisci il numero di abitanti di {nome}:");
+                abitanti = int.Parse(Console.ReadLine());
+                if (abitanti <= 0)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.Error.WriteLine("Errore: il numero di abitanti deve essere maggiore di zero.");
+                    Console.ResetColor();
+                    continue;
+                }
+                break;
+            }
+            while (true)
+            {
+                Console.WriteLine($"Inserisci il numero di abitanti over 70 di {nome}:");
+                over70 = int.Parse(Console.ReadLine());
+                if (over70 < 0 || over70 > abitanti)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.Error.WriteLine("Errore: il numero di abitanti over 70 non puo essere maggiore degli abitanti totali");
+                    Console.ResetColor();
+                    continue;
+                }
+                break;
+            }
+            perc=(double)over70/abitanti*100;
+            Console.WriteLine($"La percentuale di abitanti over 70 di {nome} e pari al {perc}%");
+        }
+    }
+}
