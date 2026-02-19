bb17ee276ae2b4f50f697b864fb4a655513bfdee
Tommy_Bstz.V2 <tommaso.bastanzetti@liceischio.edu.it>
2026-02-19 10:30:04 +0100

    Add files via upload

---
+++ random_num/random_num/Program.cs
@@ -0,0 +1,21 @@
+﻿class Program
+{
+    static void Main(){
+        int n, rnd,min,max ;
+
+        Console.WriteLine("inserire il valore minimo per la generazione del numero(>0)");
+        min = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("inserire il valore massimo per la generazione del numero(<10^32)");
+        max = int.Parse(Console.ReadLine());
+
+        rnd=new Random().Next(min,max);
+
+        do
+        {
+            Console.WriteLine($"provare a indovinare num da {min} a {max}");
+            n=int.Parse(Console.ReadLine());
+        } while (n!=rnd);
+
+    }
+}
