0eeb050783fb3143993ec849a2614ba3f3492a6e
Tommybstz <tommaso.bastanzetti@gmail.com>
2026-04-13 20:57:48 +0200

    Add positive checker for array Add Array reverser

---
+++ ArrayPositive/ArrayPositive/Program.cs
@@ -0,0 +1,23 @@
+﻿class Program
+{
+    static void Main()
+    {
+        int N;
+        Console.Write("Inserire la quantita di elementi: ");
+        while (!int.TryParse(Console.ReadLine(), out N) || N <= 0)
+        {
+            Console.Write("Per favore, inserisci un numero intero positivo: ");
+        }
+
+        int[] numbers = new int[N];
+
+        for (int i = 0; i < N; i++)
+        {
+            Console.Write($"Inserire l'elemento {i + 1}: ");
+            while (!int.TryParse(Console.ReadLine(), out numbers[i]) || numbers[i] <= 0)
+            {
+                Console.Write("Errore:inserire un numero intero positivo: ");
+            }
+        }
+    }
+}
---
+++ ArrayReverser/ArrayReverser/Program.cs
@@ -0,0 +1,27 @@
+﻿class Program
+{
+    static void Main()
+    {
+        int N;
+        Console.Write("Inserire il numero di elementi: ");
+        while (!int.TryParse(Console.ReadLine(), out N) || N <= 0)
+        {
+            Console.Write("Per favore, inserisci un numero intero positivo: ");
+        }
+
+        int[] normal = new int[N];
+        int[] reverse = new int[N];
+
+        for (int i = 0; i < N; i++) {
+            Console.WriteLine($"Inserire elemento n°{i+1}: ");
+            while (!int.TryParse(Console.ReadLine(), out normal[i]))
+            {
+                Console.Write($"Per favore, inserisci un numero intero per l'elemento n°{i+1}: ");
+            }
+            reverse[N-1-i] = normal[i];
+        }
+
+        Console.WriteLine(string.Join(", ", normal));
+        Console.WriteLine(string.Join(", ", reverse));
+    }
+}
