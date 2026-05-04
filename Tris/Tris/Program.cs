369f1361b59a6d910a40996bfe24ad921945d835
Tommybstz <tommaso.bastanzetti@gmail.com>
2026-05-04 19:50:17 +0200

    minor changes

--- Tris/Tris/Program.cs
+++ Tris/Tris/Program.cs
@@ -11,8 +11,10 @@
         {
             Console.Clear();
 
-            turn = match % 2 == 0 ? 'X' : 'Y';//assign the turn in an alternated way
+            turn = match % 2 == 0 ? 'X' : 'O';//assign the turn in an alternated way
 
+
+            Message(ConsoleColor.Cyan, "[TURNO]",$"È il turno di {turn}");
             //print the trisTable matrix as a table
             for ( x = 0; x < trisTable.GetLength(0); x++)
             {
@@ -54,8 +56,6 @@
 
             trisTable[inputX - 1, inputY - 1] = turn;
 
-
-
             //checks diagonals
             if (trisTable[0, 0] == trisTable[1, 1] && trisTable[0, 0] == trisTable[2, 2] && trisTable[0, 0] ==turn) //top-left -> bottom-right, and assign the winner X or O. 
             { 
@@ -94,15 +94,31 @@
 
         }
 
-        for(int i = 0,time=0; i < 4; i++,time+=400)
+        //print table again
+        Console.Clear();
+        Message(ConsoleColor.Cyan, "[RESULT]", "");
+        for (x = 0; x < trisTable.GetLength(0); x++)
+        {
+            Console.WriteLine(new string('─', trisTable.GetLength(1) * 4));
+            for (y = 0; y < trisTable.GetLength(1); y++)
+                Console.Write($"| {trisTable[x, y]} ");
+            Console.WriteLine("|");
+        }
+        Console.WriteLine(new string('─', trisTable.GetLength(1) * 4));
+
+        Thread.Sleep(2500);
+
+        for (int i = 0,time=0; i < 4; i++,time+=400)
         {
             Console.Clear();
             Message(ConsoleColor.Yellow, "[WINNER]", $"Il vincitore è {new string('.',i)}");
             Thread.Sleep(time);
         }
 
-        
+        Console.WriteLine();
+        Console.ForegroundColor = ConsoleColor.Yellow;
         Console.WriteLine((winner=='T'?$"Pareggio!":$"{winner}!!").PadLeft(20));
+        Console.ResetColor();
 
     }
     static void Message(ConsoleColor color,string label,string message)
