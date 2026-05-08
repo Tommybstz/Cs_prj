b3ceb71ae0632cbb6c54a89cb72c4a35216bfdc0
Tommybstz <tommaso.bastanzetti@gmail.com>
2026-05-08 18:19:04 +0200

    improved comments

--- WebScraper_tutorial/WebScraper_tutorial/Program.cs
+++ WebScraper_tutorial/WebScraper_tutorial/Program.cs
@@ -10,21 +10,21 @@ namespace WebScraper
         static void Main()
         {
             //send get request to weather.com
-            var httpClient = new HttpClient();
-            var htmlDocument= new HtmlDocument();
+            var httpClient = new HttpClient();//creates an object to send web req.
+            var htmlDocument= new HtmlDocument();//html parser
 
             //gets all the info on your position using ip-api.com
-            var json = httpClient.GetStringAsync("http://ip-api.com/json").Result;
+            var json = httpClient.GetStringAsync("http://ip-api.com/json").Result;//sends GET request to ip-api.com and returns the response as a string
             var info = JsonSerializer.Deserialize<PositionInfo>(json);//get only the lat and lon
 
 
-            string url = $"https://weather.com/it-IT/tempo/oggi/l/{info.lat},{info.lon}";
-            var html = httpClient.GetStringAsync(url).Result;
-            htmlDocument.LoadHtml(html);
+            string url = $"https://weather.com/it-IT/tempo/oggi/l/{info.lat},{info.lon}";//build url with the coordinates
+            var html = httpClient.GetStringAsync(url).Result;//same as Line 17
+            htmlDocument.LoadHtml(html);//loads the html into the parser
 
             //get the temperature
-            var temperatureElement = htmlDocument.DocumentNode.SelectSingleNode("//span[@class='CurrentConditions--tempValue--zUBSz']");
-            var temperature = temperatureElement.InnerText.Trim();
+            var temperatureElement = htmlDocument.DocumentNode.SelectSingleNode("//span[@class='CurrentConditions--tempValue--zUBSz']");//searches for the first element matching the Xpath expression
+            var temperature = temperatureElement.InnerText.Trim();//gets teh text of the element and removes whitespace
             Console.WriteLine("temperature: "+temperature);
             //get the conditions 
             var conditionElement = htmlDocument.DocumentNode.SelectSingleNode("//div[@class='CurrentConditions--phraseValue---VS-k']");
@@ -34,7 +34,7 @@ namespace WebScraper
             //get the location
             var title= htmlDocument.DocumentNode.SelectSingleNode("//title")?.InnerText.Trim();
 
-            var city = title.Split("per ")[1].Split(" - ")[0].Trim() ?? "N/A";//splits the title and takes the second part in the first split, then splits on - and takes the first part
+            var city = title.Split("per ")[1].Split(" - ")[0].Trim() ?? "N/A";//splits the title and takes the second part in the first split, then splits on - and takes the first part. so you have just the city name left
             Console.WriteLine("city: "+city);
         }
     }
